1ca46ae2960c8a7ed8f3eef1e3a6d47610eecd05
Daniel Ayala <dayala+github@protonmail.com>
2019-07-18 21:22:23 +0000

    Initial commit

---
+++ image.rb
@@ -0,0 +1,19 @@
+class Image
+  def initialize(array)
+    @image = array
+  end
+
+  def process_row(row)
+    puts row.join
+  end
+
+  def output_image
+    #@image.each {|row| process_row(row)}
+    @image.each do |row|
+      process_row(row)
+    end
+  end
+end
+
+image = Image.new([[0,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,0,0]])
+image.output_image
