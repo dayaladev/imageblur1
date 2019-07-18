155b75f0857504fcfa73efb7fd05344a6ee9a813
Daniel Ayala <dayala+github@protonmail.com>
2019-07-18 21:25:50 +0000

    Commented out example image.

--- image.rb
+++ image.rb
@@ -15,5 +15,5 @@ class Image
   end
 end
 
-image = Image.new([[0,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,0,0]])
-image.output_image
+#image = Image.new([[0,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,0,0]])
+#image.output_image
